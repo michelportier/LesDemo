e7596c2d1b31ed6d42e805866915b9ba47f957be
michel <mportier@gmail.com>
2019-03-13 16:56:16 +0100

    Met juiste deps

--- jeedemo/src/test/java/nl/oose/dea/rest/SongApiTest.java
+++ jeedemo/src/test/java/nl/oose/dea/rest/SongApiTest.java
@@ -1,11 +1,15 @@
 package nl.oose.dea.rest;
 
+import nl.oose.dea.dao.ISongDao;
+import nl.oose.dea.domain.Song;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.Response;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class SongApiTest {
@@ -13,6 +17,13 @@ public class SongApiTest {
     @Test
     public void getSongTest(){
         SongApi songApi = new SongApi();
+        ISongDao songDao = mock(ISongDao.class);
+        Song song = new Song();
+        song.setArtist("The Prodigy");
+        song.setName("Voodoo People");
+
+        when(songDao.retrieveSong(150)).thenReturn(song);
+        songApi.setSongDao(songDao);
         Response response = songApi.getSong("150");
         assertEquals(200,response.getStatus());
     }
