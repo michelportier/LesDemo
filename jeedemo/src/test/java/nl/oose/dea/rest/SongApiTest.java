6132974e01de51fbd74d795e56efb079bb3c12b6
michel <mportier@gmail.com>
2019-03-14 14:42:33 +0100

    Extra test

--- jeedemo/src/test/java/nl/oose/dea/rest/SongApiTest.java
+++ jeedemo/src/test/java/nl/oose/dea/rest/SongApiTest.java
@@ -2,6 +2,7 @@ package nl.oose.dea.rest;
 
 import nl.oose.dea.dao.ISongDao;
 import nl.oose.dea.domain.Song;
+import nl.oose.dea.rest.dto.SongDTO;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.Response;
@@ -16,15 +17,33 @@ public class SongApiTest {
 
     @Test
     public void getSongTest(){
+        String artist = "The Prodigy";
+        String songName = "Voodoo People";
         SongApi songApi = new SongApi();
         ISongDao songDao = mock(ISongDao.class);
         Song song = new Song();
-        song.setArtist("The Prodigy");
-        song.setName("Voodoo People");
+        song.setArtist(artist);
+        song.setName(songName);
 
         when(songDao.retrieveSong(150)).thenReturn(song);
         songApi.setSongDao(songDao);
         Response response = songApi.getSong("150");
+        SongDTO dto = (SongDTO) response.getEntity();
+
+        assertEquals(artist,dto.artist);
+        assertEquals(songName,dto.name);
         assertEquals(200,response.getStatus());
+    }
+
+    @Test
+    public void getSongIdDoesNotExistTest(){
+        SongApi songApi = new SongApi();
+        ISongDao songDao = mock(ISongDao.class);
+        songApi.setSongDao(songDao);
+
+        Response response = songApi.getSong("-1");
+
+
+        assertEquals(404,response.getStatus());
     }
 }
